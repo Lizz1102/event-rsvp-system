8cd653c7d88b585e8533048ddac3859c3ec1f9da
Lizz1102 <lizz05110@gmail.com>
2019-12-20 19:15:30 -0500

    Initial commit

---
+++ EventRSVPSystem/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using EventRSVPSystem.Models;
+
+
+namespace EventRSVPSystem.Controllers
+{
+		public class HomeController : Controller
+		{
+				public ViewResult Index()
+				{
+						return View("MyView");
+				}
+
+				[HttpGet]
+				public ViewResult RsvpForm()
+				{
+						return View();
+				}
+
+				[HttpPost]
+				public ViewResult RsvpForm(GuestResponse guestResponse)
+				{
+						if (ModelState.IsValid)
+						{
+								Repository.AddResponse(guestResponse);
+								return View("Thanks", guestResponse);
+						}
+						else
+						{
+								// there is a validation error
+								return View();
+						}
+				}
+
+				public ViewResult ListResponses()
+				{
+						return View(Repository.Responses.Where(r => r.WillAttend == true));
+				}
+
+		}
+}
